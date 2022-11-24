457b59c0e683bb520a80e297b8b6cb5302663660
Oliver Max <zexely@gmail.com>
2022-11-24 11:48:17 +0200

    build: added jest lib

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
+  moduleFileExtensions: ['js', 'ts'],
+  moduleDirectories: ['node_modules'],
+
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+};
